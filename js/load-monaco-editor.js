53f68e625c6ff1e12941e3b05b6a29c52a5c2951
eveningwater <854806732@qq.com>
2023-07-14 21:23:07 +0800

    feat: 修复了error

--- js/load-monaco-editor.js
+++ js/load-monaco-editor.js
@@ -1,16 +1,16 @@
 require.config({
-    paths: { vs: '//cdn.jsdelivr.net/npm/monaco-editor@latest/min/vs' }
+  paths: { vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@latest/min/vs' }
 });
 window.MonacoEnvironment = { getWorkerUrl: () => proxy };
 
 let proxy = URL.createObjectURL(
-    new Blob(
-        [
-            `
-          self.MonacoEnvironment = {baseUrl: '//cdn.jsdelivr.net/npm/monaco-editor@latest/min/'};
-	        importScripts('//cdn.jsdelivr.net/npm/monaco-editor@latest/min/vs/base/worker/workerMain.js');
+  new Blob(
+    [
+      `
+            self.MonacoEnvironment = {baseUrl: 'https://cdn.jsdelivr.net/npm/monaco-editor@latest/min/'};
+	        importScripts('https://cdn.jsdelivr.net/npm/monaco-editor@latest/min/vs/base/worker/workerMain.js');
         `
-        ],
-        { type: 'text/javascript' }
-    )
+    ],
+    { type: 'text/javascript' }
+  )
 );
